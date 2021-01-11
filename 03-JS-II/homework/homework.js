2a29a56a2cbe4b64769dafa50a6f8fbecd933610
Fidel Hugo Angel Tolaba <einsh2018@gmail.com>
2021-01-11 01:18:49 -0300

    CP commiteado por einsh a las 1610338728744

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -20,7 +20,7 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
   if(idioma === 'aleman'){
-    return 'Guten Tag';
+    return 'Guten Tag!';
   } else if( idioma === 'mandarin'){
     return 'Ni Hao!';
   } else if( idioma === 'ingles'){
@@ -60,7 +60,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if
+  return numero % 1 === 0;
 }
 
 function fizzBuzz(numero) {
@@ -68,6 +68,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if( numero % 3 === 0 && numero % 5 === 0){
+    return 'fizzbuzz';
+  } else if(numero % 5 === 0){
+    return 'buzz';
+  } else if(numero % 3 === 0){
+    return 'fizz';
+  } else if (!(numero % 15 === 0)){
+    return numero;
+  }
 }
 
 function esPrimo(numero) {
@@ -76,6 +85,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
