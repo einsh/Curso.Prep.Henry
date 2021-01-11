58d5e036df12fe59c1fccc595e3b90e990b6c8a5
Fidel Hugo Angel Tolaba <einsh2018@gmail.com>
2021-01-10 23:08:35 -0300

    CP commiteado por einsh a las 1610330914130

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,11 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x > y){
+    return x;
+  } else {
+    return y;
+  }
 }
 
 function saludo(idioma) {
@@ -14,18 +19,37 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === 'aleman'){
+    return 'Guten Tag';
+  } else if( idioma === 'mandarin'){
+    return 'Ni Hao!';
+  } else if( idioma === 'ingles'){
+    return 'Hello!';
+  } else {
+    return 'Hola!'
+  }
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(numero === 10 || numero === 5){
+      return true;
+    } else  {
+      return false;
+    }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero < 50 && numero > 20){
+    return true;
+  } else  {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -36,6 +60,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if
 }
 
 function fizzBuzz(numero) {
